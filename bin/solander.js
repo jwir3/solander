5946b89a50a87f52da8877b4ba64f047b17e3f2b
Scott Johnson <jaywir3@gmail.com>
2018-03-17 11:20:55 -0500

    Add ntc library and add basic handling for converting a color via CLI.

--- bin/solander.js
+++ bin/solander.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
-var solander = require('../dist/solander');
+const process = require('process');
+const path = require('path');
+const Solander = require('../dist/solander');
 
-solander();
+function printUsage(errorMsg) {
+  if (errorMsg) {
+    console.error('Error: ' + errorMsg + "\n");
+  }
+
+  console.error("usage: " + path.basename(process.argv[1]) + " <color>\n");
+  console.error("Retrieve the name of a hexidecimal color using a conventional naming mechanism.\n");
+  console.error("\tcolor: A color in hexidecimal form (as a string).")
+}
+
+if (process.argv.length < 3) {
+  printUsage("You must specify a hexidecimal color, optionally preceded by a hash");
+}
+
+let args = [ process.argv[2] ];
+let solander = new Solander(args);
+console.log("Color Name: " + solander.colorName);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
-function main () {
-  console.log("Hello, World!");
+
+const ntc = require('../lib/ntc');
+
+class Solander {
+  constructor(colors) {
+    this._colorName = null;
+
+    // Take argument (assuming it's a hex notation)
+    if (colors.length != 1) {
+      throw new Error("You must pass a hexadecimal color, optionally preceded by a hash mark");
+    }
+
+    // Send to library for color name
+    this._colorName = this._lookupColorName(this._interpretAsHexColor(colors[0]));
+  }
+
+  get colorName() {
+    if (!this._colorName) {
+      throw new Error("No color was initialized; Color name cannot be returned.");
+    }
+
+    return this._colorName;
+  }
+
+  _interpretAsHexColor(color) {
+    // Aside from the '#' mark, there should be either three or six characters.
+    let interpretedColor = color;
+    if (color.startsWith('#')) {
+      interpretedColor = color.slice(1);
+    }
+
+    // TODO: Check that the hex color is valid.
+
+    if (interpretedColor.length === 3) {
+      return interpretedColor[0] + interpretedColor[0]
+             + interpretedColor[1] + interpretedColor[1]
+             + interpretedColor[2] + interpretedColor[2];
+    } else if (interpretedColor.length === 6) {
+      return interpretedColor;
+    } else {
+      throw new Error("Expected color to have 3 or 6 characters, not including the # sign. It had: " + interpretedColor.length);
+    }
+  }
+
+  _lookupColorName(color) {
+    var nameMatch = ntc.name('#' + color);
+    if (nameMatch) {
+      return nameMatch[1];
+    }
+
+    return "Unknown";
+  }
 }
 
-module.exports = main;
+module.exports = Solander;
